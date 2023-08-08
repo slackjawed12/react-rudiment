ba8bd05dd41222db504cb552996445807df1994a
Tony Lee <tony@treasurer.co.kr>
2023-08-08 14:46:37 +0900

    feature(tictactoe): Square 컴포넌트가 props를 받음

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,24 +1,31 @@
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log("clicked!");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
