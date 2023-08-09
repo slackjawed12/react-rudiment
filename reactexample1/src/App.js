b811516e238c1439f67aa628e1aeffd4499e7dbf
narustudio <slackjawed12@gmail.com>
2023-08-09 20:53:27 +0900

    feature(reactexample1): 인라인 css 적용

--- reactexample1/src/App.js
+++ reactexample1/src/App.js
@@ -1,15 +1,29 @@
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 import MyHeader from "./MyHeader";
 import MyFooter from "./MyFooter";
 import React from "react";
 
 function App() {
+  let name = "홍길동";
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
   return (
-    <div className="App">
+    <div className="App" style={style.App}>
       <MyHeader />
-      <h2>Hello React</h2>
-      <b id="bold_text">react.js</b>
+      <h2 style={style.h2}>Hello React, I'm {name}</h2>
+      <b style={style.bold_text} id="bold_text">
+        react.js
+      </b>
       <MyFooter />
     </div>
   );
