6f6fcabd3bf83c243fe2a07e047ca369a7b05559
narustudio <slackjawed12@gmail.com>
2023-08-09 20:44:18 +0900

    feature(reactexample1): header, footer 추가

--- reactexample1/src/App.js
+++ reactexample1/src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MyHeader from "./MyHeader";
+import MyFooter from "./MyFooter";
 
 function App() {
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">안녕 리액트</header>
+      <MyFooter />
     </div>
   );
 }
