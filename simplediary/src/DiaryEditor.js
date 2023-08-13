be23a51b32d40688e90c353f5146a81d68eba172
narustudio <slackjawed12@gmail.com>
2023-08-13 20:24:46 +0900

    feat(simplediary): 입력 길이 제한 조건을 핸들러에 추가한다.

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -17,7 +17,17 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    // 빈 입력을 검증한다.
+    if (state.author.length < 1) {
+      alert("작성자는 최소 1글자 이상 입력해주세요.");
+      return;
+    }
+
+    if (state.content.length < 5) {
+      alert("일기 본문은 최소 5글자 이상 입력해주세요.");
+      return;
+    }
+
     alert("저장 성공");
   };
 
