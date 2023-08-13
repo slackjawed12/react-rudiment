f5b301790fea4f7636613540e8ae67e1be7c41f8
narustudio <slackjawed12@gmail.com>
2023-08-13 21:25:52 +0900

    feat(simplediary):DiaryItem 컴포넌트에서 props를 활용해서 요소를 반환한다

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,5 +1,16 @@
-const DiaryItem = () => {
-  return <div className="DiaryItem"></div>;
+const DiaryItem = ({ author, content, createdAt, emotion, id }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(createdAt).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
 };
 
 export default DiaryItem;
