3926754a9d4810455845a0a1cf267c2f6c374536
narustudio <slackjawed12@gmail.com>
2023-08-13 22:47:33 +0900

    feat(simplediary):수정중인 일기 내용 state 추가

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 const DiaryItem = ({ author, content, createdAt, emotion, id, onRemove }) => {
   const [isEdit, setIsEdit] = useState(false);
   const toggleIsEdit = () => setIsEdit(!isEdit);
-
+  // 수정 중인 내용
+  const [localContent, setLocalContent] = useState("");
   const handleRemove = () => {
     if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
       onRemove(id);
@@ -18,7 +19,18 @@ const DiaryItem = ({ author, content, createdAt, emotion, id, onRemove }) => {
         <br />
         <span className="date">{new Date(createdAt).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
       <button onClick={handleRemove}>삭제하기</button>
       <button onClick={toggleIsEdit}>수정하기</button>
     </div>
