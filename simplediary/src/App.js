ac2481d3b0833c53f2adc24da54a421e2a64c0fa
narustudio <slackjawed12@gmail.com>
2023-08-13 20:56:22 +0900

    feat(simplediary): 일기 리스트 컴포넌트 추가

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
   return (
     <div className="App">
       <h2>일기장</h2>
       <DiaryEditor />
+      <DiaryList />
     </div>
   );
 }
