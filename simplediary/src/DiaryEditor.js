38d12470dd33188ae6a877068fbbc59e517484d4
narustudio <slackjawed12@gmail.com>
2023-08-13 20:19:20 +0900

    feature(simplediary): 저장 버튼을 추가한다.

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -16,6 +16,11 @@ const DiaryEditor = () => {
     });
   };
 
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공");
+  };
+
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
@@ -45,6 +50,9 @@ const DiaryEditor = () => {
           <option value={4}>4</option>
           <option value={5}>5</option>
         </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
