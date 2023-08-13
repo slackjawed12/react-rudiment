0228553c3b640dd69c7e884e7dcad2b2e1c7078e
narustudio <slackjawed12@gmail.com>
2023-08-13 22:43:20 +0900

    feat(simplediary):handleRemove로 핸들러 분리, 수정버튼 핸들러 추가

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,4 +1,14 @@
+import { useState } from "react";
 const DiaryItem = ({ author, content, createdAt, emotion, id, onRemove }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -9,15 +19,8 @@ const DiaryItem = ({ author, content, createdAt, emotion, id, onRemove }) => {
         <span className="date">{new Date(createdAt).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onRemove(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <button onClick={handleRemove}>삭제하기</button>
+      <button onClick={toggleIsEdit}>수정하기</button>
     </div>
   );
 };
