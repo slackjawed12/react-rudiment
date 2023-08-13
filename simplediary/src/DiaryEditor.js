a336ec34fa43ddbc71ae671f83d10a6779f1fe3c
narustudio <slackjawed12@gmail.com>
2023-08-13 19:49:20 +0900

    feature(simplediary): 본문 textarea element 추가

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 const DiaryEditor = () => {
   const [author, setAuthor] = useState("글쓴이 이름");
-
+  const [content, setContent] = useState("본문");
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
@@ -12,6 +12,14 @@ const DiaryEditor = () => {
             console.log(e.target.value);
             console.log(e.target.name);
             setAuthor(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
           }}
         />
       </div>
