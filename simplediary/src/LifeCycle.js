4888f95ccfabf5c462409e96b284b31f7a744b5c
narustudio <slackjawed12@gmail.com>
2023-08-15 22:28:10 +0900

    feat(simplediary): lifecycle 컴포넌트 추가 - useEffect 사용

---
+++ simplediary/src/LifeCycle.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const LifeCycle = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("mount!");
+  }, []);
+
+  useEffect(() => {
+    console.log("update!");
+  });
+
+  return (
+    <div style={{ padding: 20 }}>
+      <div>
+        {count}
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+    </div>
+  );
+};
+
+export default LifeCycle;
