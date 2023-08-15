7f3cfe00e1112e13739fd0bc8dafd2c58a53513d
narustudio <slackjawed12@gmail.com>
2023-08-15 22:38:21 +0900

    feat(simplediary): unmount 시 나타나게 할 컴포넌트 추가

--- simplediary/src/LifeCycle.js
+++ simplediary/src/LifeCycle.js
@@ -1,39 +1,16 @@
 import React, { useEffect, useState } from "react";
 
+const UnmountTest = () => {
+  return <div>Unmount Testing Component</div>;
+};
 const LifeCycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  // dependency array에 있는 state가 변화하면 callback을 호출한다.
-  useEffect(() => {
-    console.log("mount!");
-  }, []);
-
-  useEffect(() => {
-    console.log("update!");
-  });
-
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
-      setCount(1);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    console.log(`text is update : ${text}`);
-  }, [text]);
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
