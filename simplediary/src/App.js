f4829aacc52e25a6011f939cfa072a9c1e796d3a
narustudio <slackjawed12@gmail.com>
2023-08-16 22:00:42 +0900

    feat(simplediary): 부모 컴포넌트의 상태 변화에 따른 자식 컴포넌트 렌더링 테스트

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import OptimizeTest from "./OptimizeTest";
 
 // const dummyList = [
 //   {
@@ -69,7 +70,6 @@ function App() {
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
@@ -84,8 +84,6 @@ function App() {
 
   // data.length가 변할때만 callback이 호출된다.
   const getDiaryAnalysis = useMemo(() => {
-    console.log("일기 분석 시작");
-
     const goodCount = data.filter((it) => it.emotion >= 3).length;
     const badCount = data.length - goodCount;
 
@@ -97,6 +95,7 @@ function App() {
 
   return (
     <div className="App">
+      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
---
+++ simplediary/src/OptimizeTest.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const CountView = ({ count }) => {
+  useEffect(() => {
+    console.log(`update :: count : ${count}`);
+  });
+  return <div>{count}</div>;
+};
+
+const TextView = ({ text }) => {
+  useEffect(() => {
+    console.log(`update :: text : ${text}`);
+  });
+  return <div>{text}</div>;
+};
+const OptimizeTest = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  return (
+    <div style={{ padding: 50 }}>
+      <div>
+        <h2>count</h2>
+        <CountView count={count} />
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>
+        <h2>text</h2>
+        <TextView text={text} />
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+    </div>
+  );
+};
+
+export default OptimizeTest;
