f25d787af8fcc554c3e8de2c43afaf3b91192e8f
narustudio <slackjawed12@gmail.com>
2023-08-23 22:57:49 +0900

    feat(emotiondiary): MyButton의 prop으로 정의되지 않은 타입이 들어올 때 예외처리

--- emotiondiary/src/components/MyButton.js
+++ emotiondiary/src/components/MyButton.js
@@ -1,7 +1,10 @@
 export const MyButton = ({ text, type, onClick }) => {
+  // 없는 타입이 들어올 때 예외 처리
+  const btnType = ["positive", "negative"].includes(type) ? type : "default";
+
   return (
     <button
-      className={["MyButton", `MyButton_${type}`].join(" ")}
+      className={["MyButton", `MyButton_${btnType}`].join(" ")}
       onClick={onClick}
     >
       {text}
