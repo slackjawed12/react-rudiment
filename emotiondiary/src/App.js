de438e1fbccb905afc8446b1bb867b62caa36234
narustudio <slackjawed12@gmail.com>
2023-08-23 23:09:59 +0900

    feat(emotiondiary): MyHeader 추가, css 작성, App.js에 추가

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -7,6 +7,7 @@ import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import RouteTest from "./components/RouterTest";
 import { MyButton } from "./components/MyButton";
+import { MyHeader } from "./components/MyHeader";
 
 function App() {
   const env = process.env;
@@ -14,6 +15,18 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
         <MyButton
           text={"버튼"}
---
+++ emotiondiary/src/components/MyHeader.js
@@ -0,0 +1,10 @@
+export const MyHeader = ({ headText, leftChild, rightChild }) => {
+  return (
+    // header 태그이기 때문에 className 필요없음
+    <header>
+      <div className="head_btn_left">{leftChild}</div>
+      <div className="head_text">{headText}</div>
+      <div className="head_btn_right">{rightChild}</div>
+    </header>
+  );
+};
