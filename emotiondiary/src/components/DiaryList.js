499f44e7b10de23d7e5028de0125c8c4a749e11a
narustudio <slackjawed12@gmail.com>
2023-09-06 23:17:29 +0900

    feat(emotiondiary): 리스트 필터링 컴포넌트 제작 및 필터링 로직 구현

--- emotiondiary/src/components/DiaryList.js
+++ emotiondiary/src/components/DiaryList.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신 순" },
+  { value: "oldest", name: "오래된 순" },
+];
+/**
+ * 일기 리스트 필터링 컴포넌트
+ */
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, index) => (
+        <option key={index} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("latest");
+
+  const getProcessedDiaryList = () => {
+    // diaryList deep copy
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((it) => {
-        <div key={it.id}>{it.content}</div>;
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessedDiaryList().map((it) => {
+        return <div key={it.id}>{it.content}</div>;
       })}
     </div>
   );
