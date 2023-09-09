20ad7af5d8996bd8b94ee02d932c1aae7f8a608a
narustudio <slackjawed12@gmail.com>
2023-09-09 22:17:11 +0900

    feature(emotiondiary): New Page의 컴포넌트 내용의 중복 방지 - DiaryEditor 컴포넌트 추가

--- emotiondiary/src/pages/New.js
+++ emotiondiary/src/pages/New.js
@@ -1,36 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import MyButton from "../components/MyButton";
-import MyHeader from "../components/MyHeader";
-import { useState } from "react";
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-  const [date, setDate] = useState(getStringDate(new Date()));
-  const navigate = useNavigate();
   return (
     <div>
-      <MyHeader
-        headText={"새 일기쓰기"}
-        leftChild={
-          <MyButton onClick={() => navigate(-1)} text={"< 뒤로가기"} />
-        }
-      />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
-            <input
-              className="input-date"
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
 };
