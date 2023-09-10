b5622f8246742eec0ce2aecc71002b20d029f720
narustudio <slackjawed12@gmail.com>
2023-09-10 22:41:47 +0900

    feature(emotiondiary): 일기 삭제 처리 및 마운트 시 로컬스토리지 데이터로 초기화

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -31,56 +31,31 @@ const reducer = (state, action) => {
       return state;
   }
 
+  localStorage.setItem("diary", JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "오늘의 일기 1번",
-    date: 1694008162346,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: "오늘의 일기 2번",
-    date: 1694008162396,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: "오늘의 일기 3번",
-    date: 1694008162496,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: "오늘의 일기 4번",
-    date: 1694008162596,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: "오늘의 일기 5번",
-    date: 1694008229503,
-  },
-];
+
 function App() {
   useEffect(() => {
-    const item1 = localStorage.getItem("item1");
-    const item2 = localStorage.getItem("item2");
-    const item3 = JSON.parse(localStorage.getItem("item3"));
-    console.log({ item1, item2, item3 });
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      dispatch({ type: "INIT", data: diaryList });
+    }
   }, []);
 
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
 
   console.log(new Date().getTime());
 
-  const dataId = useRef(6);
+  const dataId = useRef(0);
   // CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
@@ -97,7 +72,7 @@ function App() {
 
   // REMOVE
   const onRemove = (targetId) => {
-    dispatch({ type: "REMOVE", targetId });
+    dispatch({ type: "REMOVE", data: { id: targetId } });
   };
 
   // EDIT
